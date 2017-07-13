6dbdfdcc355af5265b76d2f9bc9722736b0628c2
TAN BEE LAN <tp032074@mail.apu.edu.my>
2017-07-13 23:07:44 +0800

    Add project files.

---
+++ MaerskLineWeb/Models/Shipyard.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MaerskLineWeb.Models
+{
+    public class Shipyard
+    {
+        public int ShipyardID { get; set; }
+
+        public String Shipyard_Name { get; set; }
+
+        public String Shipyard_Location { get; set; }
+    }
+}
